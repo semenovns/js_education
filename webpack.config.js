4a6b37457e0075e48a594a3fa765f2b16802987f
semenovns <semenov.ns@mail.ru>
2022-10-06 13:50:35 +0600

    Added webpack-html-plugin,
    added webpack server

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,26 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
 module.exports = {
-    
+    entry: path.resolve(__dirname, 'src/testingScript.js'),
+   
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: '[name].[contenthash].js',
+      },
+      mode : 'production',
+      plugins: [
+        new HtmlWebpackPlugin({
+          title: 'Js Education Page',
+          filename: 'index.html',
+          template: 'src/index.html'
+        })
+      ],
+      devServer: {
+        static: {
+          directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
+        port: 9000,
+      },
 }
